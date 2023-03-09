7d1ba8ed3456f5be33c81d527ed7d4dd3564ce2f
Vitor Lucas Fujita Felício <vlucasff@hotmail.com>
2023-03-09 13:58:27 -0300

    add

--- arquivos/uploadArquivos/recebedor.php
+++ arquivos/uploadArquivos/recebedor.php
@@ -1,5 +1,13 @@
 <?php
 echo '<pre>';
-print_r($_FILES);
-$nome = $_FILES['arquivos']['name'];
-move_uploaded_file($_FILES['arquivo']['tmp_name'], 'arquivos/'.$nome);
+print_r( $_FILES );
+
+if(in_array($_FILES['arquivo']['type'], array('image.jpeg', 'image/jpg', 'image/png'))){
+    echo '<br/>arquivo está sendo salvo...';
+    $nome = md5(time().rand(0,1000)).'.jpg';
+    move_uploaded_file($_FILES['arquivo']['tmp_name'], 'arquivos/'.$nome);
+    echo '<br/>arquivo salvo com sucesso';
+}else{
+    echo 'arquivo não permitido';
+}
+
